4f7aa8d92e90ce39b763c08847fd8e188dc835dd
Katana Coleman <131917030+MPsilky@users.noreply.github.com>
2023-11-08 19:15:06 -0500

    Update 004_create_movies_theaters.js

--- src/db/migrations/004_create_movies_theaters.js
+++ src/db/migrations/004_create_movies_theaters.js
@@ -1,5 +1,7 @@
 exports.up = function(knex) {
-    return knex.schema.createTable("movies_theaters", (table) => {
+    return knex.schema.hasTable("movies_theaters").then(function(exists) {
+    if (!exists) {
+      return knex.schema.createTable("movies_theaters", (table) => {
           table.integer("movie_id").unsigned().notNullable();
           table
               .foreign("movie_id")
@@ -20,3 +22,5 @@ exports.up = function(knex) {
   exports.down = function(knex) {
     return knex.schema.dropTable("movies_theaters");
   };
+    }
+  });
